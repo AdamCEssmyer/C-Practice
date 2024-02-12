6f0ac13e6bad0cdb42e0f0a8eb3fe934f1338ed9
Adam Essmyer <acemyer@gmail.com>
2024-02-12 07:53:58 -0600

    created subscription renewal function

--- TestProjects/Program.cs
+++ TestProjects/Program.cs
@@ -1,32 +1,28 @@
-Random dice = new Random();
+Random random = new Random();
 
-int roll1 = dice.Next(1,7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if (daysUntilExpiration == 0)
 {
-
-    if((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
+    Console.WriteLine("Your subscriber has expired.");
 }
-else {}
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if(daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-
-if (total >= 15)
+if (discountPercentage > 0)
 {
-    Console.WriteLine("You win!");
-}
-else
-{
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine($"Renew now and save{discountPercentage}%.");
 }
