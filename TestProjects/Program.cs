bd37be576c2a287b1ae76bc6ca45d4785e39cb77
Adam Essmyer <acemyer@gmail.com>
2024-02-24 10:53:26 -0600

    created nested iteration and selection statements

--- TestProjects/Program.cs
+++ TestProjects/Program.cs
@@ -1,28 +1,8 @@
-Random random = new Random();
-
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-if (daysUntilExpiration == 0)
+string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string orderID in orderIDs)
 {
-    Console.WriteLine("Your subscriber has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if(daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save{discountPercentage}%.");
+    if(orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
 }
