1bff4c7fe5b540fc512053e891b7eba037b0b69e
Adam Essmyer <acemyer@gmail.com>
2024-04-23 10:18:09 -0500

    code readability practice with converting message into array

--- TestProjects/Program.cs
+++ TestProjects/Program.cs
@@ -1,8 +1,13 @@
-string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-foreach (string orderID in orderIDs)
-{
-    if(orderID.StartsWith("B"))
-    {
-        Console.WriteLine(orderID);
-    }
-}
+string str = "The quick brown fox";
+//convert message into a char array
+char[] charMessage = str.ToCharArray();
+//reverse the chars
+Array.reverse(charMessage);
+int x = 0;
+//count the o's
+foreach (char i in charMessage) {if (i == 'o') {x++; }}
+//convert it back to a string
+string new_message = new string(charMessage);
+//print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
