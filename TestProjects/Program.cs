96731b265405cc4f72b27e6ea37dacd23c2c58e5
Adam Essmyer <acemyer@gmail.com>
2024-04-24 14:03:25 -0500

    created new console project

--- TestProjects/Program.cs
+++ TestProjects/Program.cs
@@ -11,3 +11,21 @@ string new_message = new string(charMessage);
 //print it out
 Console.WriteLine(new_message);
 Console.WriteLine($"'o' appears {x} times.");
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 3;
+        int num3;
+
+        //unary operators
+        num3 = num1;
+        Console.WriteLine("num3 is {0}", num3);
+        
+        bool isSunny  = true;
+        Console.WriteLine("is it sunny? {0}", !isSunny);
+    }
+}
